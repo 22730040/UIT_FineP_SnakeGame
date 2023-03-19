39291e2b73c857a07d0a3fe9f08243c4dfb62309
Khang Huynh <dellta103@gmail.com>
2023-03-19 15:32:54 +0700

    delete unused files

--- api/src/routers/users.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const { createUser } = require('../../functions/users')
-const userRouter = express.Router()
-
-userRouter.post('/create', createUser)
-
-module.exports = userRouter
